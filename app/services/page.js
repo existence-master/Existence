2c484c472b17be4dfa08d6b360166d7a700cd737
Kabeer2004 <merchantkabeer68@gmail.com>
2024-07-19 02:34:54 +0530

    one more bug fix in footer

--- app/services/page.js
+++ app/services/page.js
@@ -518,7 +518,7 @@ const Services = () => {
           <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
             2024{" "}
-            <a href="https://flowbite.com/" class="hover:underline">
+            <a href="/" class="hover:underline">
               Existence
             </a>
             . All Rights Reserved.
